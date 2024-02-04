686da7f7e049dac662ae897337c06c776f929aa2
mc373 <106958276+mc373@users.noreply.github.com>
2024-02-04 21:19:09 +0100

    pull test

--- src/app/searchmain/page.tsx
+++ src/app/searchmain/page.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import ConditionList from "./conditonlist/ConditonList";
 import ResTable from "./restable/ResTable";
 const SearchMain = () => {
-  return <div>a{/* <ConditionList /> */}</div>;
+  return <div>a{<ConditionList />}</div>;
 };
 
 export default SearchMain;
